18fba517d2cbb7fc7d1a131dcab84c686915c51d
LI Yuyang <lyybsni@gmail.com>
2020-03-14 22:04:39 +0800

    Initial Commit

---
+++ WhatToEat2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatToEat2.0
+//
+//  Created by lyy on 14/3/2020.
+//  Copyright © 2020 City-University-Of-Hong-Kong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
